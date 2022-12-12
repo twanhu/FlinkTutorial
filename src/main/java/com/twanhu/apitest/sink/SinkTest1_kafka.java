ac40c92419ff21baf2cfd9a24af54ba44fa346ee
twanhu <twanhu@.qq.com>
2022-12-12 12:48:57 +0800

    version 2

--- src/main/java/com/twanhu/apitest/sink/SinkTest1_kafka.java
+++ src/main/java/com/twanhu/apitest/sink/SinkTest1_kafka.java
@@ -16,14 +16,26 @@ public class SinkTest1_kafka {
         // 设置执行任务的并行度
         env.setParallelism(3);
 
-        // 配置kafka相关的属性，Properties : 用于读取Java的配置文件，其配置文件常为.properties文件，是以键值对的形式进行参数配置的
-        Properties properties = new Properties();
-        properties.setProperty("zookeeper.connect", "master:2181,slave1:2181,slave2:2181");
-        properties.setProperty("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");
-        properties.setProperty("group.id", "consumer-group");
+        // 配置Kafka Producer相关的属性，Properties : 用于读取Java的配置文件，其配置文件常为.properties文件，是以键值对的形式进行参数配置的
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty("zookeeper.connect", "master:2181,slave1:2181,slave2:2181");
+        producerProperties.setProperty("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");
+        // 序列化
+        producerProperties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // 配置Kafka Consumer相关的属性
+        Properties consumerProperties = new Properties();
+        consumerProperties.setProperty("zookeeper.connect", "master:2181,slave1:2181,slave2:2181");
+        consumerProperties.setProperty("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");
+        consumerProperties.setProperty("group.id", "consumer");
+        // 反序列化
+        consumerProperties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProperties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProperties.setProperty("auto.offset.reset", "latest");
 
         // 从kafka读取数据,kafka传输数据的格式为字节序列，SimpleStringSchema 对其序列化
-        DataStream<String> inputStream = env.addSource(new FlinkKafkaConsumer011<String>("sensor", new SimpleStringSchema(), properties));
+        DataStream<String> inputStream = env.addSource(new FlinkKafkaConsumer011<String>("sensor", new SimpleStringSchema(), consumerProperties));
 
         // 转换成SensorReading类型
         DataStream<String> dataStream = inputStream.map(line -> {
@@ -32,7 +44,7 @@ public class SinkTest1_kafka {
         });
 
         // 将数据发送到kafka,kafka传输数据的格式为字节序列，SimpleStringSchema 对其序列化
-        dataStream.addSink(new FlinkKafkaProducer011<String>("sinktest", new SimpleStringSchema(),properties));
+        dataStream.addSink(new FlinkKafkaProducer011<String>("sinktest", new SimpleStringSchema(),producerProperties));
 
         env.execute();
     }
--- src/main/java/com/twanhu/apitest/source/SourceTest2_kafka.java
+++ src/main/java/com/twanhu/apitest/source/SourceTest2_kafka.java
@@ -17,8 +17,10 @@ public class SourceTest2_kafka {
         // 配置kafka相关的属性，Properties : 用于读取Java的配置文件，其配置文件常为.properties文件，是以键值对的形式进行参数配置的
         Properties properties = new Properties();
         properties.setProperty("zookeeper.connect", "master:2181,slave1:2181,slave2:2181");
-        properties.setProperty("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");
-        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("bootstrap.serversc", "master:9092,slave1:9092,slave2:9092");
+        // 序列化
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         // 从kafka读取数据,kafka传输数据的格式为字节序列，SimpleStringSchema 对其序列化
         DataStream<String> dataStream = env.addSource(new FlinkKafkaConsumer011<String>("sensor", new SimpleStringSchema(), properties));
